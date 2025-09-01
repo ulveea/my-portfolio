38cde2f875d77edd548a2522f30235ca444afa07
Ulviyya Niftiyeva <uteimur03@gmail.com>
2025-09-01 23:52:40 +0400

    intro button

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./style.module.scss";
+import { Link } from 'react-router-dom';
 
 const Intro = () => (
   <div className={styles.intro}>
     <h1 className={styles.title}>Hi, I'm Ulviyya Niftiyeva</h1>
     <p className={styles.subtitle}>Front-end Developer & Project Manager</p>
-    <a href="/portfolio" className={styles.cta}>See my projects</a>
-  </div>
+     <Link to="/portfolio" className={styles.cta}>
+                        See my projects
+     </Link>  </div>
 );
 
 export default Intro;
